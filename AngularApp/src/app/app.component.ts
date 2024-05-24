cd68b1c6edb7bf53ffccb7359fbcac445da8622f
keerthikmr <keerthikumar.m10@gmail.com>
2024-05-24 18:19:45 +0530

    Add http imports

--- AngularApp/src/app/app.component.ts
+++ AngularApp/src/app/app.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { FormsModule, NgForm } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
-import { HttpClientModule, HttpClient } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
 import { EmpAddFormComponent } from './emp-add-form/emp-add-form.component';
 import { EmpDisplayComponent } from './emp-display/emp-display.component';
@@ -9,26 +8,11 @@ import { EmpDisplayComponent } from './emp-display/emp-display.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [EmpDisplayComponent, RouterOutlet, FormsModule, HttpClientModule, CommonModule, EmpAddFormComponent],
+  imports: [EmpDisplayComponent, RouterOutlet, FormsModule, CommonModule, EmpAddFormComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'AngularApp';
-  API_URL = 'http://localhost:8000';
-
-  employees: any = [];
-  new_employee: any = {};
-
-  constructor(private http: HttpClient) {}
-
-  ngOnInit() {
-    this.getEmployees();
-  }
-
-  getEmployees() {
-    this.http.get(this.API_URL + '/get_all_employees').subscribe(data => {
-      this.employees = data;
-    });
-  }
+  
 }
