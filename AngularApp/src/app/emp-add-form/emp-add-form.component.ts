3814470712cf87f96dcddc4b85c35e2dc891f678
keerthikmr <keerthikumar.m10@gmail.com>
2024-05-27 14:03:33 +0530

    Fix date formatting

--- AngularApp/src/app/emp-add-form/emp-add-form.component.ts
+++ AngularApp/src/app/emp-add-form/emp-add-form.component.ts
@@ -8,6 +8,7 @@ import { MatInputModule } from '@angular/material/input';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
 import {MatButtonModule} from '@angular/material/button';
+import { DateAdapter } from '@angular/material/core';
 
 @Component({
   selector: 'app-emp-add-form',
@@ -19,7 +20,9 @@ import {MatButtonModule} from '@angular/material/button';
 export class EmpAddFormComponent implements OnInit {
   userForm: FormGroup = new FormGroup({});
 
-  constructor(private fb: FormBuilder, private http: HttpClient) { }
+  constructor(private fb: FormBuilder, private http: HttpClient, private dateAdapter: DateAdapter<Date>) {
+    this.dateAdapter.setLocale('en-GB');
+  }
 
   ngOnInit() {
     this.userForm = this.fb.group({
@@ -32,10 +35,18 @@ export class EmpAddFormComponent implements OnInit {
   }
 
   formattedDate(ogDate: Date) {
-    const date = ogDate.getDate();
-    const month = ogDate.getMonth();
-    const year = ogDate.getFullYear();
-    return `${year}-${month}-${date}`;
+    // const date = ogDate.getDate();
+    // const month = ogDate.getMonth();
+    // const year = ogDate.getFullYear();
+    // return `${year}-${month}-${date}`;
+
+    const result = ogDate.toLocaleDateString("fr-CA", {
+      year: "numeric",
+      month: "2-digit",
+      day: "2-digit",
+    })
+    console.log(result);
+    return result;
   }
 
   onSubmit(form: FormGroup ) {
@@ -45,8 +56,8 @@ export class EmpAddFormComponent implements OnInit {
       const formData = new FormData();
       formData.append('first_name', form.value.first_name);
       formData.append('last_name', form.value.last_name);
-      formData.append('birth_date', this.formattedDate(form.value.birth_date));
       formData.append('gender', form.value.gender);
+      formData.append('birth_date', this.formattedDate(form.value.birth_date));
       formData.append('hired_date', this.formattedDate(form.value.hired_date));
   
       this.http.post('http://localhost:8000/add_employee', formData).subscribe((response) => {
