b98596a157820d97d61af2f8d659c62a100b391d
keerthikmr <keerthikumar.m10@gmail.com>
2024-06-03 15:05:01 +0530

    Redirect home to employees

--- AngularApp/src/app/app.routes.ts
+++ AngularApp/src/app/app.routes.ts
@@ -8,7 +8,7 @@ import { TitleAddComponent } from './title-add/title-add.component';
 import { TitleDisplayComponent } from './title-display/title-display.component';
 
 export const routes: Routes = [
-    { path: '', component: EmpDisplayComponent },
+    { path: 'home', component: EmpDisplayComponent },
     { path: 'employees', component: EmpDisplayComponent },
     { path: 'employees/add', component: EmpAddFormComponent },
     { path: 'departments', component: DepartmentDisplayComponent },
