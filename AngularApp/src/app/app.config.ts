96943fca2b6d59bfba73c490fb6f94c12c3d3a3a
keerthikmr <keerthikumar.m10@gmail.com>
2024-06-11 12:20:21 +0530

    Use withfetch in http provider

--- AngularApp/src/app/app.config.ts
+++ AngularApp/src/app/app.config.ts
@@ -4,15 +4,9 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { HTTP_INTERCEPTORS, provideHttpClient, withFetch, withInterceptorsFromDi, withInterceptors } from '@angular/common/http';
+import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
 import { AuthenticationInterceptor } from './services/interceptor';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration(), provideAnimationsAsync(), provideHttpClient(withInterceptors([AuthenticationInterceptor]))]
-    // {
-    //   provide: HTTP_INTERCEPTORS,
-    //   useClass: AuthenticationInterceptor,
-    //   multi: true
-    // }
-  // ]
+  providers: [provideRouter(routes), provideClientHydration(), provideAnimationsAsync(), provideHttpClient(withFetch(), withInterceptors([AuthenticationInterceptor]))]
 };
