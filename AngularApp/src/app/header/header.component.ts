8aea4f88d37d85a82a3f3c300c73a9ca74e27c9e
keerthikmr <keerthikumar.m10@gmail.com>
2024-06-11 12:19:08 +0530

    Set up profile window overlay

--- AngularApp/src/app/header/header.component.ts
+++ AngularApp/src/app/header/header.component.ts
@@ -2,21 +2,49 @@ import { Component } from '@angular/core';
 import { RouterOutlet, RouterLink } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
 import { MatIcon } from '@angular/material/icon';
+import { NgIf } from '@angular/common';
+import { Router } from '@angular/router';
+import { MatButton } from '@angular/material/button';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterOutlet, RouterLink, MatIcon],
+  imports: [RouterOutlet, RouterLink, MatIcon, NgIf, MatButton],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
 
-  constructor(private http: HttpClient) {}
+  profile: any = {};
+  profileShown: boolean = false;
+
+  constructor(private http: HttpClient, private router: Router) {}
 
   viewProfile() {
+
+    if (this.profileShown) {
+      this.closeProfile();
+    } else {
+      this.showProfile();
+    }
+  }
+
+  showProfile() {  
     this.http.get('http://localhost:8000/api/Auth/get_logged_user').subscribe(data => {
-      console.log(data);
+    
+      this.profile = data;
+      console.log(this.profile);
     });
+    this.profileShown = true;
+  }
+
+  closeProfile() {
+    this.profileShown = false;
+  }
+
+  logout() {
+    localStorage.removeItem('jwtToken');
+    this.closeProfile();
+    this.router.navigate(['/login']);
   }
 }
